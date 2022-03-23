50091ad04d72515a7165c146669e1d5b25ae1a6e
Yuliya Sidarenka <yulka1102@gmail.com>
2022-03-23 11:23:47 +0100

    Portfolio.jsx and Portfolio.css

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "./Portfolio.css"
 import {data} from "../../data";
 import PortfolioItem from "./PortfolioItem";
+import { Pagination, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Portfolio = () => {
     return (
         <section id={'portfolio'}>
             <h5>My Recent Works</h5>
             <h2>Portfolio</h2>
-
-            <div className="container portfolio__container">
-                {data.map(item => <PortfolioItem key={item.id} item={item}/>)}
-            </div>
+                <Swiper
+                    pagination={{
+                        type: "fraction",
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    className="container portfolio__container"
+                >
+                    {data.map(item => <SwiperSlide>
+                        <PortfolioItem key={item.id} item={item}/>
+                    </SwiperSlide>)}
+                </Swiper>
         </section>
     );
 }
